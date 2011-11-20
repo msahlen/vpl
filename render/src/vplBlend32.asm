4938bea2e30c6ec199ae7368454f7c8b6fb9860e
Mattias Sahlen <mattias.sahlen@gmail.com>
2011-11-20 08:58:33 +0100

    Some work on SSE blending

--- render/src/vplBlend32.asm
+++ render/src/vplBlend32.asm
@@ -2,14 +2,15 @@ section	.text
 
 ; Exported functions
 global blendSrc
+global blendSrcSolid
 
 ; C prototype;
-; void blendSrc(vplUint32* dest,vplUint32* color,vplUint count)
-; dest is pointer to destination
-; color is pointer to 4 32bit colors
+; All blend functions look like:
+; void blendSolid(vplUint32* dest,vplUint32 color,vplUint count)
+; void blend(vplUint32* dest,vplUint32* src,vplUint count)
 ; count is how many pixels that should be blended, this always at least 8
- 
-blendSrc:
+
+blendSrcSolid:
 
       ; Get arguments
       enter 0,0
@@ -17,11 +18,12 @@ blendSrc:
       push ebx
       push ecx
       mov eax, [ebp + 8]  ; dst pointer in eax
-      mov ebx, [ebp + 12] ; color pointer in ebx
+      mov ebx, [ebp + 12] ; color in ebx
       mov ecx, [ebp + 16] ; number of pixels in ecx
 
-      ; Load data
-      movaps xmm0,[ebx]  ; xmm0 = color color color color 
+      ; Load and shuffle data
+      movaps xmm0,ebx      ; xmm0 = 0 0 0 color
+      shufps xmm0,xmm0,0x0 ; xmm0 = color color color color
 
 doFour:
       movaps [eax],xmm0  ; Write result
@@ -43,13 +45,66 @@ doTwo:
       ;Increase address, decrease remaining pixels
       sub ecx, 2
       add eax, 8
-     
+
 doOne:
 	  cmp ecx,0x0
 	  je blendEnd
-	  
+
 	  mov ecx,[ebx]
 	  mov [eax],ecx
+
+blendEnd:
+
+      pop eax
+      pop ebx
+      pop ecx
+      leave
+      ret
+
+; Blend src, color is not solid
+blendSrc:
+
+      ; Get arguments
+      enter 0,0
+      push eax
+      push ebx
+      push ecx
+      mov eax, [ebp + 8]  ; dst pointer in eax
+      mov ebx, [ebp + 12] ; src pointer in ebx
+      mov ecx, [ebp + 16] ; number of pixels in ecx
+
+doFour:
+
+      movaps xmm0,[ebx] ; xmm0 = color color color color
+
+      sub ecx, 4         ; Decrease remaining pixels
+      movaps [eax],xmm0  ; Write result
+
+      ; Increase pointers
+      add eax, 16
+      add ebx, 16
+
+      ; Loop ?
+      cmp ecx,0x4
+      jge doFour
+
+      cmp ecx,0x1
+      jle doOne
+
+doTwo:
+       movq xmm0,[ebx] ; xmm0 = 0 0 color color
+       sub ecx, 2      ;Decrease remaining pixels
+       movq [eax],xmm0 ; Write result
+       add eax, 8      ; Increase pointer
+
+doOne:
+      ; are we done?
+      cmp ecx,0x0
+      je blendEnd
+
+      ; write last pixel
+      mov ecx,[ebx]
+      mov [eax],ecx
 
 blendEnd:
 
--- render/src/vplBlend64.asm
+++ render/src/vplBlend64.asm
@@ -4,21 +4,24 @@ section	.text
 
 ; Exported functions
 global blendSrc
+global blendSrcSolid
 
 ; C prototype;
-; void blendSrc(vplUint32* dest,vplUint32* color,vplUint count)
-; dest is pointer to destination
-; color is pointer to 4 32bit colors
+; All blend functions look like:
+; void blendSolid(vplUint32* dest,vplUint32* color,vplUint count)
+; void blend(vplUint32* dest,vplUint32* src,vplUint count)
 ; count is how many pixels that should be blended, this always at least 8
- 
-blendSrc:
+
+; Src blend, solid color
+blendSrcSolid:
 
       enterFunction
 
-      ; Load data
-      movaps xmm0,[ARG2]  ; xmm0 = color color color color 
+      ; Load data and shuffle data
+      movd xmm0,[ARG2]     ; xmm0 = 0 0 0 color
+      shufps xmm0,xmm0,0x0 ; xmm0 = color color color color
 
-doFour:
+doFourSolid:
       movaps [ARG1],xmm0  ; Write result
 
       ;Increase address, decrease remaining pixels
@@ -27,24 +30,67 @@ doFour:
 
       ; Loop ?
       cmp ARG3,0x4
-      jge doFour
+      jge doFourSolid
 
 	  cmp ARG3,0x1
-      jle doOne
+      jle doOneSolid
 
-doTwo:
+doTwoSolid:
 	  movq [ARG1],xmm0  ; Write result
 
       ;Increase address, decrease remaining pixels
       sub ARG3, 2
       add ARG1, 8
-     
-doOne:
+
+doOneSolid:
 	  cmp ARG3,0x0
-	  je blendEnd
-	  
+	  je blendEndSolid
+
 	  mov ARG3,[ARG2]
 	  mov [ARG1],ARG3
+
+blendEndSolid:
+
+      leaveFunction
+
+
+; Blend src, color is not solid
+blendSrc:
+
+      enterFunction
+
+doFour:
+
+      movaps xmm0,[ARG2] ; xmm0 = color color color color
+
+      sub ARG3, 4         ; Decrease remaining pixels
+      movaps [ARG1],xmm0  ; Write result
+
+      ; Increase pointers
+      add ARG1, 16
+      add ARG2, 16
+
+      ; Loop ?
+      cmp ARG3,0x4
+      jge doFour
+
+      cmp ARG3,0x1
+      jle doOne
+
+doTwo:
+       movq xmm0,[ARG2] ; xmm0 = 0 0 color color
+       sub ARG3, 2      ;Decrease remaining pixels
+       movq [ARG1],xmm0 ; Write result
+       add ARG1, 8      ; Increase pointer
+
+doOne:
+      ; are we done?
+      cmp ARG3,0x0
+      je blendEnd
+
+      ; write last pixel
+      mov ARG3,[ARG2]
+      mov [ARG1],ARG3
 
 blendEnd:
 
